6a422e61806ffad72957444160d9b189fe67ab34
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-06 23:59:55 +0800

    perf(reader): prefer instant paging for new profiles

--- apps/jingdu/android/app/src/main/java/com/junchen/jingdu/ReaderPreferences.kt
+++ apps/jingdu/android/app/src/main/java/com/junchen/jingdu/ReaderPreferences.kt
@@ -91,7 +91,7 @@ data class ReaderSettings(
     val emphasizeHeadings: Boolean = true,
     val preset: ReaderPreset = ReaderPreset.STANDARD,
     val readingMode: ReaderMode = ReaderMode.PAGED,
-    val pageAnimation: ReaderPageAnimation = ReaderPageAnimation.SLIDE,
+    val pageAnimation: ReaderPageAnimation = ReaderPageAnimation.NONE,
     val tapPagingEnabled: Boolean = true,
     val swipePagingEnabled: Boolean = true,
     val reversePagingGestures: Boolean = false,
@@ -227,7 +227,7 @@ private fun ChineseDisplayModeProto.toModel() = ChineseDisplayMode.entries.getOr
 private fun ReaderMode.toProto() = ReaderModeProto.values()[ordinal]
 private fun ReaderModeProto.toModel() = ReaderMode.entries.getOrElse(ordinal) { ReaderMode.PAGED }
 private fun ReaderPageAnimation.toProto() = ReaderPageAnimationProto.values()[ordinal]
-private fun ReaderPageAnimationProto.toModel() = ReaderPageAnimation.entries.getOrElse(ordinal) { ReaderPageAnimation.SLIDE }
+private fun ReaderPageAnimationProto.toModel() = ReaderPageAnimation.entries.getOrElse(ordinal) { ReaderPageAnimation.NONE }
 private fun ReaderOrientation.toProto() = ReaderOrientationProto.values()[ordinal]
 private fun ReaderOrientationProto.toModel() = ReaderOrientation.entries.getOrElse(ordinal) { ReaderOrientation.SYSTEM }
 private fun ReaderTextAlignment.toProto() = ReaderTextAlignmentProto.values()[ordinal]
